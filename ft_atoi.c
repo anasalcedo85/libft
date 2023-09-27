88ce1ba46874d96786c2fe296e7bd96bdb5361df
AnaSalcedo <ansalced@student.42malaga.com>
2023-09-27 21:24:48 +0200

    revisados por paco

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,30 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansalced <ansalced@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 19:36:29 by ansalced          #+#    #+#             */
+/*   Updated: 2023/09/27 21:09:29 by ansalced         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+int	ft_isspace(char c)
+{
+	if (c == ' ')
+		return (1);
+	else if (c == '\t')
+		return (1);
+	else if (c == '\n')
+		return (1);
+	else if (c == '\r')
+		return (1);
+	else if (c == '\v')
+		return (1);
+	else if (c == '\f')
+		return (1);
+	else
+		return (0);
+}
 
 int	ft_atoi(const char *str)
 {
-	int i;
+	int	i;
 	int	n;
-	int signo;
+	int	signo;
 
 	i = 0;
 	n = 0;
 	signo = 1;
-
-	while(str[i] == ' ')
-	{
+	while (ft_isspace(str[i]))
 		i++;
-	}
-
-	if(str[i] == '-')
+	if (str[i] == '-')
 	{
 		signo = -1;
 		i++;
 	}
-	
+	else if (str[i] == '+')
+		i++;
 	while (ft_isdigit(str[i]))
 	{
 		n = n * 10 + (str[i] - '0');
-
 		i++;
 	}
 	return (n * signo);
--- libft.h
+++ libft.h
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansalced <ansalced@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 20:46:03 by ansalced          #+#    #+#             */
+/*   Updated: 2023/09/27 21:19:17 by ansalced         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
 
-#include <stddef.h>
-
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isascii(int c);
-int ft_isalnum(int c);
-int ft_isprint(int c);
-int ft_strlen(char *str);
-int	ft_islower(int c);
-int	ft_isupper(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+int		ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_atoi(const char *str);
 
 #endif
