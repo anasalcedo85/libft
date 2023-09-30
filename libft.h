8f0eb88b78b1081c11f33d4632627fc218745f7d
AnaSalcedo <ansalced@student.42malaga.com>
2023-09-30 13:58:12 +0200

    actualizacion 30 sept

--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -25,5 +26,7 @@ int		ft_isupper(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *str);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
 
 #endif
